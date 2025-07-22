eb00f50c1d10b7a967e000df3c5e2b646b3e0f5d
jignaciodegiovanni <jignaciodegiovanni@gmail.com>
2025-07-21 23:35:55 -0300

    Hmm

--- SSE-BluePill/Debug/Core/Lib/ili9341/fonts.d
+++
@@ -1,3 +0,0 @@
-Core/Lib/ili9341/fonts.o: ../Core/Lib/ili9341/fonts.c \
- ../Core/Lib/ili9341/fonts.h
-../Core/Lib/ili9341/fonts.h:
--- SSE-BluePill/Debug/ExtSrc/Util.d
+++
@@ -1,4 +0,0 @@
-ExtSrc/Util.o: \
- C:/Users/Juan/STM32CubeIDE/workspace_1.16.0/Common/Src/Util.c \
- ../../Common/Inc/Util.h
-../../Common/Inc/Util.h:
